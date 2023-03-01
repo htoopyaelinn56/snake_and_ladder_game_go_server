ccf3654424b3fc548d93989d5dec9c56a32b6919
htoopyaelinn56 <hplinn0@gmail.com>
2023-03-01 18:42:53 +0630

    change fiber

--- go.mod
+++ go.mod
@@ -2,17 +2,31 @@ module main
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.9.0
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/fasthttp/websocket v1.5.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/tinylib/msgp v1.1.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.44.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+)
 
 require (
 	github.com/bytedance/sonic v1.8.0 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/gorilla/websocket v1.5.0
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/gofiber/websocket/v2 v2.1.4
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- src/server.go
+++ src/server.go
@@ -1,92 +1,43 @@
 package src
 
 import (
-	"encoding/json"
-	"log"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
 )
-
-type response struct {
-	Current int `json:"current"`
-	Dice    int `json:"dice_num"`
-}
 
 var clients []*websocket.Conn
 
-func handleWebSocket(c *gin.Context) {
-	// Upgrade the connection to a websocket connection
-	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Fatal("Error upgrading connection to websocket: ", err)
-	}
-
-	// Send a welcome message to the client
-	err = ws.WriteMessage(websocket.TextMessage, []byte("Welcome!"))
-	if err != nil {
-		log.Println("Error sending welcome message to client: ", err)
-	}
-
-	// Add the new client to the clients slice
-	clients = append(clients, ws)
-
-	// Start listening for incoming messages from the client
-	go dice(ws)
-}
-
-func dice(ws *websocket.Conn) {
+func handWs(c *websocket.Conn) {
+	clients = append(clients, c)
 	for {
-		// Read a message from the client
-		_, message, err := ws.ReadMessage()
+		_, message, err := c.ReadMessage()
 		if err != nil {
-			log.Println("Error reading message from client: ", err)
-			removeClient(ws)
+			fmt.Println("read error:", err)
 			break
 		}
-		var incomingJson *response
-		decodeErr := json.Unmarshal(message, &incomingJson)
-		// Send the message to all connected clients
-		if decodeErr != nil {
-			log.Println("json decode error")
-		}
-		sendToAllClients(incomingJson, ws)
+		fmt.Printf("received: %s\n", message)
+
+		go handleMessages(message)
 	}
 }
 
-func sendToAllClients(message *response, ws *websocket.Conn) {
+func handleMessages(data []byte) {
 	for _, client := range clients {
-		if ws != client {
-			err := client.WriteJSON(message)
-
-			if err != nil {
-				log.Println("Error sending message to client: ", err)
-				removeClient(client)
-			}
-		}
-
-	}
-}
-
-func removeClient(ws *websocket.Conn) {
-	// Remove the client from the clients slice
-	for i, client := range clients {
-		if client == ws {
-			clients = append(clients[:i], clients[i+1:]...)
+		err := client.WriteMessage(1, data)
+		if err != nil {
+			fmt.Println("write error:", err)
 			break
 		}
 	}
 }
 
 func RunApp() {
-	// Configure routes
-	r := gin.Default()
-	r.GET("/dice", handleWebSocket)
+	app := fiber.New()
 
-	// Start the server
-	log.Println("Starting server...")
-	err := r.Run()
-	if err != nil {
-		log.Fatal("Error starting server: ", err)
-	}
+	// WebSocket route
+	app.Get("/ws", websocket.New(handWs))
+
+	app.Listen(":3000")
 }
